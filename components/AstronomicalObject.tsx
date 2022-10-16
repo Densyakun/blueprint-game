bd2bc1bb3e64220141531c1e10509dcbfa79e06f
densyakun <densyakun1128@gmail.com>
2022-10-16 18:18:17 +0900

    Boxes move to AstronomicalObject.tsx

---
+++ components/AstronomicalObject.tsx
@@ -0,0 +1,31 @@
+import * as THREE from 'three'
+import React, { useRef, useState } from 'react'
+import { useFrame, ThreeElements } from '@react-three/fiber'
+
+function Box(props: ThreeElements['mesh']) {
+  const mesh = useRef<THREE.Mesh>(null!)
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+  useFrame((state, delta) => (mesh.current.rotation.x += 1.57 * delta))
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
+
+export default function AstronomicalObject() {
+  return (
+    <>
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,7 @@
 import Head from 'next/head'
-import * as THREE from 'three'
-import React, { useRef, useState } from 'react'
-import { Canvas, useFrame, ThreeElements } from '@react-three/fiber'
-
-function Box(props: ThreeElements['mesh']) {
-  const mesh = useRef<THREE.Mesh>(null!)
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
-  useFrame((state, delta) => (mesh.current.rotation.x += 1.57 * delta))
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? 1.5 : 1}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
-  )
-}
+import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import AstronomicalObject from '../components/AstronomicalObject'
 
 export default function Page() {
   return (
@@ -34,8 +15,7 @@ export default function Page() {
         <Canvas>
           <ambientLight />
           <pointLight position={[10, 10, 10]} />
-          <Box position={[-1.2, 0, 0]} />
-          <Box position={[1.2, 0, 0]} />
+          <AstronomicalObject />
         </Canvas>
       </div>
     </>
